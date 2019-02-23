b0dd40ba3eefef2a6b3b7712d301937f12e1e37f
Alvaro Orozco <aoroz032@fiu.edu>
2019-02-23 17:02:12 -0500

    db models

--- djreact/backend/src/books/models.py
+++ djreact/backend/src/books/models.py
@@ -1,11 +1,93 @@
 from django.db import models
 
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    home_address = models.CharField(max_length=200)
+    date_added = models.DateField(auto_now_add=True)
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    bio = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+    
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+
+class ShippingInformation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=200)
+    default = models.BooleanField(default=False)
+    date_added = models.DateField(auto_now_add=True)
+
+class CreditCard(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    number = models.CharField(max_length=100)
+    expdate = models.CharField(max_length=50)
+    holdername = models.CharField(max_length=200)
+    seccode = models.CharField(max_length=50)
+    billing_address = models.CharField(max_length=200)
+    date_added = models.DateField(auto_now_add=True)
 
 class Book(models.Model):
-    bookID = models.IntegerField( default = 0 ) 
-    title = models.CharField(max_length=120)
-    content = models.TextField()
-    cover = models.ImageField( null = True, blank=True, upload_to="book_covers/" )
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    cover = models.FilePathField(path='browsing/images/covers')
+    description = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
 
-    def __str__(self):
-        return self.title
+class Publishing(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    publisher = models.CharField(max_length=200)
+    release_date = models.DateField()
+    date_added = models.DateField(auto_now_add=True)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    shownick = models.BooleanField(default=True)
+    comment = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    book = models.ForeignKey(Book, on_delete = models.CASCADE)
+    shownick = models.BooleanField(default=True)
+    starts = models.IntegerField()
+    date_added = models.DateField(auto_now_add=True)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    book = models.ForeignKey(Book, on_delete = models.CASCADE)
+    quantity = models.IntegerField()
+    savedlater = models.BooleanField(default=False)
+    date_added = models.DateField(auto_now_add=True)
+
+class WishList(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    date_added = models.DateField(auto_now_add=True)
+
+class WishListDetails(models.Model):
+    wishlist = models.ForeignKey(WishList, on_delete = models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    date_added = models.DateField(auto_now_add=True)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    totalprice = models.IntegerField()
+    date_added = models.DateField(auto_now_add=True)
+
+class OrderDetails(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    totalprice = models.IntegerField()
