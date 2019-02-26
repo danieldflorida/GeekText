6308bb28f8071949cd69b8ad311c049fbb89daf9
Daniel Ochoa <docho008@gmail.com>
2019-02-25 20:08:36 -0500

    imported css files inside their correct .js files

--- djreact/frontend/gui/src/components/BookDetail.js
+++ djreact/frontend/gui/src/components/BookDetail.js
@@ -1,6 +1,6 @@
 import React from 'react' 
 import MyVerticallyCenteredModal from './EnlargedCover'
-
+import '../stylingFiles/BookDetail.css'
 
 
 
--- djreact/frontend/gui/src/components/EnlargedCover.js
+++ djreact/frontend/gui/src/components/EnlargedCover.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Modal from 'react-bootstrap/Modal';
 
 import '../stylingFiles/EnlargedCover.css'
-import '../stylingFiles/BookDetail.css'
+
 
 
 
