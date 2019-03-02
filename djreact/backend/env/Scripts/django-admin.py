87fe74b43cf7fcba0455d0c69df5d97ba11ab1ff
Sean Palmer <42584813+spalm023@users.noreply.github.com>
2019-03-01 19:15:14 -0500

    Remove duplicated directory

--- djreact/backend/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\zelot\desktop\djreact\backend\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- djreact/backend/src/cart/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import CartItemsView
-
-urlpatterns = [ 
-    #path( '', CartItemsView.as_view() ),
-    #path( '', CartDetailView.as_view() ),
-
-]
