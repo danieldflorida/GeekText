1cbd656cfb6b8747f97e2ce024fb65a4bd5aefca
Sean Palmer <42584813+spalm023@users.noreply.github.com>
2019-04-06 23:58:47 -0400

    Changed button on book detail page to point to a cart number saved in state.

--- djreact/frontend/gui/src/book-details/containers/BookDetailView.js
+++ djreact/frontend/gui/src/book-details/containers/BookDetailView.js
@@ -4,7 +4,8 @@ import axios from 'axios'
 class BookDetail extends React.Component {
 
     state = { 
-        book: {}
+        book: {},
+        currentUser: 7
     }
 
     componentDidMount() {
@@ -18,12 +19,12 @@ class BookDetail extends React.Component {
     }
 
     handleClickCart() { 
-        axios.put("http://127.0.0.1:8000/carts/7/add_to_cart/", `{"book_id":${this.state.book.bookID}}`,{headers: {"Content-Type": "application/json"}}  )
+        axios.put(`http://127.0.0.1:8000/carts/${this.state.currentUser}/add_to_cart/`, `{"book_id":${this.state.book.bookID}}`,{headers: {"Content-Type": "application/json"}}  )
         alert( "Item had been added to your cart." )
     }
 
     handleClickSave() { 
-        axios.put("http://127.0.0.1:8000/carts/7/save_later/", `{"book_id":${this.state.book.bookID}}`,{headers: {"Content-Type": "application/json"}}  )
+        axios.put(`http://127.0.0.1:8000/carts/${this.state.currentUser}/save_later/`, `{"book_id":${this.state.book.bookID}}`,{headers: {"Content-Type": "application/json"}}  )
         alert( "Item had been saved for later. View it in your cart." )
     }
 
